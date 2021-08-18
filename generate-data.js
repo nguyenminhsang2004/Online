9742abf0d3f42541a8354fb62b5b154c66ee4156
nguyenminhsang2004 <67231653+nguyenminhsang2004@users.noreply.github.com>
2021-08-18 23:02:59 +0700

    FC

---
+++ generate-data.js
@@ -0,0 +1,68 @@
+
+const faker = require('faker');
+const fs = require('fs');
+
+faker.locale = "vi";
+
+
+randomCitiesList = () => {
+    const citiesList = [];
+    ["TP Hồ Chí Minh","Hà Nội","Tây Ninh","Bình Dương","Long An"].map(value => {
+        const city = {
+            id: faker.random.uuid(),
+            name: value,
+            createdAt: Date.now(),
+            updatedAt: Date.now()
+        }
+
+        citiesList.push(city);
+    })
+
+    return citiesList;
+}
+
+randomStudentList = (citiesList, numberOfCity) => {
+    if(numberOfCity <= 0) return [];
+    const studentList = [];
+
+    for (const city of citiesList) {
+        Array.from(new Array(numberOfCity)).forEach(() => {
+            const student = {
+                cityId: city.id,
+                id: faker.random.uuid(),
+                name: faker.name.findName(),
+                sex: faker.name.gender(),
+                dob: faker.date.past(),
+                phone: faker.phone.phoneNumberFormat(),
+                address: faker.address.streetAddress(),
+                score: parseFloat((Math.random() * (10 - 5) + 5).toFixed(2)),
+                imageUrl: faker.image.imageUrl(400,400),
+                createdAt: Date.now(),
+                updatedAt: Date.now()
+            }
+
+            studentList.push(student);
+        })
+    }
+
+    return studentList;
+
+}
+
+
+// IFFE
+(() => {
+
+    const citiesList = randomCitiesList();
+    const studentsList = randomStudentList(citiesList, 5);
+
+    const db = {
+        cities:citiesList,
+        students: studentsList
+    }
+
+    fs.writeFile('db.json',JSON.stringify(db),
+    () => {
+        console.log("Write successfully");
+    });
+})()
